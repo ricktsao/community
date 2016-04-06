1ce9219735624a386e854ceddb45ef17d70bcdd2
claire <myinfo.huang@gmail.com>
2016-04-06 23:05:18 +0800

    update db config

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = '27.147.4.239';
 $db['default']['port'] = 3306;
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'admin';
 //$db['default']['password'] = '';
 $db['default']['database'] = 'community';
 
