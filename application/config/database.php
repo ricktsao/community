9094cf8374cebdf21f724f6d27e87ab02a1cb7af
Rick <ricktsao@gmail.com>
2016-04-08 09:47:08 +0800

    update

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = '192.168.1.45';
-$db['default']['port'] = 3307;
+$db['default']['hostname'] = '27.147.4.239';
+$db['default']['port'] = 3306;
 $db['default']['username'] = 'root';
-$db['default']['password'] = '12120808';
+$db['default']['password'] = 'admin';
 //$db['default']['password'] = '';
 $db['default']['database'] = 'community';
 
