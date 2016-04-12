dec565d0f542050a44fb5875724d416ad99b3028
Rick <ricktsao@gmail.com>
2016-04-12 23:33:55 +0800

    首頁新版套版

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -12,9 +12,12 @@ class Home extends Frontend_Controller {
 	public function index()
 	{
 		$this->addCss("css/index.css");
+		
+		$this->addJs("js/jquery-2.2.3.min.js");
 		$this->addJs("js/jquery.cycle2.min.js");
 		$this->addJs("js/jquery.cycle2.carousel.min.js");
-		
+		$this->addJs("js/fancybox/lib/jquery.mousewheel-3.0.6.pack.js");
+		$this->addJs("js/fancybox/source/jquery.fancybox.pack.js?v=2.1.5");		
 		
 		$data = array();
 		
--- application/views/frontend/template_index_view.php
+++ application/views/frontend/template_index_view.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
 	<title><?php echo tryGetData("website_title",$webSetting);?></title>
     <!-- 全站 -->
-    <link rel="stylesheet" href="<?php echo base_url().$templateUrl;?>css/default.css">
-	<script src="<?php echo base_url().$templateUrl;?>js/jquery-1.12.2.min.js"></script>
+	<link rel="stylesheet" href="<?php echo base_url().$templateUrl;?>css/default.css">
+	<link rel="stylesheet" href="<?php echo base_url().$templateUrl;?>js/fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+	<link rel="stylesheet" href="<?php echo base_url().$templateUrl;?>css/font-awesome/css/font-awesome.min.css">
 	
 	<!-- 本頁使用-->
 	<?php echo $style_css;?>
@@ -20,8 +21,17 @@
 
 <?php echo $footer;?>
 
-
-
+<script>
+$(function() 
+{
+    $("#member_login_btn").fancybox();
+	$('#cycle').cycle({
+		slides: ">a",
+		fx: "carousel",
+		pager:"#pager"
+	});
+})
+</script>
 </body>
 </html>
 
