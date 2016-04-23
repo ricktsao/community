5930797f8dfb91ac4bdea2c817e2e014a8249c72
claire <myinfo.huang@gmail.com>
2016-04-23 17:43:46 +0800

    db config fix

--- application/config/database.php
+++ application/config/database.php
@@ -48,7 +48,7 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = '27.147.4.239';
+$db['default']['hostname'] = 'localhost';
 $db['default']['port'] = 3306;
 $db['default']['username'] = 'root';
 $db['default']['password'] = 'admin';
