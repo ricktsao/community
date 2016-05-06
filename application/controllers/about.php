a4997728c1016c63b06aadcaf4c40617c8f6f9ac
Rick <ricktsao@gmail.com>
2016-05-06 20:23:05 +0800

    關於社區

---
+++ application/controllers/about.php
@@ -0,0 +1,36 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class About extends Frontend_Controller {
+
+
+	function __construct() 
+	{
+		parent::__construct();
+		$this->displayBanner(FALSE);  	
+	}
+
+	
+	public function index()
+	{
+
+		
+		$item_info = $this->c_model->GetList( "about");			
+
+			
+		if($item_info["count"]>0)
+		{				
+			img_show_list($item_info["data"],'img_filename',"about");
+			
+			$data["item_info"] = $item_info["data"][0];			
+		}
+		else 
+		{
+			$data["item_info"] = array();
+		}
+		
+		
+		
+		$this->display("detail_view",$data);
+	}
+}
+
---
+++ application/views/frontend/about/detail_view.php
@@ -0,0 +1,13 @@
+<div class="primary">
+	
+    <div class="form_group">
+	<div class="form_page_title">關於社區</div>		
+		<div class="form_page_text">
+		<?php echo tryGetData("content",$item_info);?>
+		</div>
+		<div class="form_page_img">
+		<img src="<?php echo tryGetData("img_filename",$item_info);?>" >
+		</div>
+		<br>		
+	</div>
+</div>
